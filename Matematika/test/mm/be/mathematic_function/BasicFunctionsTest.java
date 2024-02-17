62b77fcfc89ebeafb2c3b20faba6d33aa1112aab
Alien696 <michaelmrlik@gmail.com>
2024-02-17 18:57:10 +0100

    revert changes

--- Matematika/test/mm/be/mathematic_function/BasicFunctionsTest.java
+++ Matematika/test/mm/be/mathematic_function/BasicFunctionsTest.java
@@ -19,23 +19,23 @@ class BasicFunctionsTest {
 
     @Test
     void addTwoNumbers() {
-        assertEquals(954, basicFunctions.addTwoNumbers(256, 698));
+        assertEquals(954, basicFunctions.addTwoNumbers(256,698));
 
 
     }
 
     @Test
     void substractTwoNumbers() {
-        assertEquals(-5, basicFunctions.substractTwoNumbers(10, 15));
+        assertEquals(-442,basicFunctions.substractTwoNumbers(256,698));
     }
 
     @Test
     void multiplyTwoNumbers() {
-        assertEquals(178688, basicFunctions.multiplyTwoNumbers(256, 698));
+        assertEquals(178688,basicFunctions.multiplyTwoNumbers(256,698));
     }
 
     @Test
     void divideTwoNumbers() {
-        assertTrue(String.valueOf(basicFunctions.divideTwoNumbers(256, 698)).contains("0.3667621"));
+        assertTrue(String.valueOf(basicFunctions.divideTwoNumbers(256,698)).contains("0.3667621"));
     }
 }
