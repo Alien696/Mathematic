ce1d766c8995c0108269d5e00bd7609adab9844d
Alien696 <michaelmrlik@gmail.com>
2024-03-14 21:32:19 +0100

    Implemented backend logic

--- Matematika/src/mm/fe/SwingApplication.java
+++ Matematika/src/mm/fe/SwingApplication.java
@@ -1,5 +1,7 @@
 package mm.fe;
 
+import mm.be.mathematic_function.BasicFunctions;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,13 +10,16 @@ public class SwingApplication {
     private JPanel outputPanel;
     private JPanel buttonsPanel;
     private JTextField output;
+    private int firstNumber;
+    private int secondNumber;
+    private String mark = "";
 
     public void createBasicWindow() {
         JFrame frame = new JFrame();
         frame.setSize(450, 500);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-//        frame.setResizable(false);
+        frame.setResizable(false);
 
         // POZOR záleží na pořadí metoda addPanels musí být volána před komponentama
         addPanels(frame);
@@ -40,11 +45,11 @@ public class SwingApplication {
     }
 
     private void addFunctionButtons() {
-        addButtons(4,4);
+        addButtons(4, 4);
     }
 
     private void addNumericButtons() {
-        addButtons(3,4);
+        addButtons(3, 4);
     }
 
     private void addButtons(int rowCount, int columnCount) {
@@ -68,8 +73,55 @@ public class SwingApplication {
 //        Počítáme s tím že událost bude pouze z JButton proto si můžeme dovolit přetypovat zdroj na JButton
         JButton button = (JButton) evt.getSource(); // měkke přetypování = Pokud mám nějakého předka můžu jej pomocí kulatých závorek přetypovat na jednoho z jeho potomků
         String buttonText = button.getText();
+        if (buttonText.equals("+")) {
+            firstNumber = Integer.parseInt(output.getText());
+            mark = "+";
+            output.setText("");
+        } else if (buttonText.equals("-")) {
+            firstNumber = Integer.parseInt(output.getText());
+            mark = "-";
+            output.setText("");
+        } else if (buttonText.equals("*")) {
+            firstNumber = Integer.parseInt(output.getText());
+            mark = "*";
+            output.setText("");
+        } else if (buttonText.equals("/")) {
+            firstNumber = Integer.parseInt(output.getText());
+            mark = "/";
+            output.setText("");
+
+
+        } else if (buttonText.equals("=")) {
+            secondNumber = Integer.parseInt(output.getText());
+            final BasicFunctions basicFunctions = new BasicFunctions();
+            int result = 0;
+            if (mark.equals("+")) {
+                result = basicFunctions.addTwoNumbers(firstNumber, secondNumber);
+
+            } else if (mark.equals("-")) {
+                result = basicFunctions.substractTwoNumbers(firstNumber, secondNumber);
+            } else if (mark.equals("*")) {
+                result = basicFunctions.multiplyTwoNumbers(firstNumber, secondNumber);
+            } else if (mark.equals("/")) {
+                showInDisplay("" + basicFunctions.divideTwoNumbers(firstNumber, secondNumber));
+                return;
+            }
+            showInDisplay("" + result);
+
+        } else {
+            addInToDisplay(buttonText);
+
+        }
+    }
+
+    private void showInDisplay(String text) {
+        output.setText(text);
+    }
+
+    private void addInToDisplay(String buttonText) {
         output.setText(output.getText() + buttonText);
-//        output.setText(((JButton)evt.getSource()).getText());
+
+//        output.setText(output.getText()+((JButton)evt.getSource()).getText()); Toto je to samé jako 3 řádky nad tím a pouze je to napsané v jednom řádku
 
     }
 
@@ -80,9 +132,9 @@ public class SwingApplication {
         } else if (resultName.equals("-1")) {
             return "=";
         } else if (resultName.equals("10")) {
-            return "÷";
+            return "/";
         } else if (i == 3 && j == 1) {
-            return "×";
+            return "*";
         } else if (i == 3 && j == 2) {
             return "-";
         } else if (i == 3 && j == 3) {
